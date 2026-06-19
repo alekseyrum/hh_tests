a2356083028964ce624bf875f5cb459cd823aad8
alekseyrum <r.a.o@inbox.ru>
2026-06-19 17:17:11 +0400

    bugfix C++ Средний практика - Задача 1

--- code_sources_c++/HHMedium01.cpp
+++ code_sources_c++/HHMedium01.cpp
@@ -7,12 +7,19 @@
 #include <map>
 using namespace std;
 
-std::vector<std::string> generate_report(const std::vector<std::string> & lines) 
+std::vector<std::string> generate_report(const std::vector<std::string> & input) 
 {
     std::vector<std::string> vecResult;
     std::map<string, int> mapWorkers;
 
-    for (const std::string &word : lines) {
+    for (const std::string &prepareLine : input) {
+
+        string word;
+        word.reserve(prepareLine.length() );
+        for(auto  curC : prepareLine ) {        // remove all SPACEs
+            if ( curC != ' ') word.push_back( curC );
+        }
+
         std::string_view sv(word);
         int delim1 = sv.find(';');
         int delim2 = sv.find(';', delim1+1);
@@ -26,17 +33,17 @@ std::vector<std::string> generate_report(const std::vector<std::string> & lines)
         std::string_view svProject = sv.substr(delim3+1, sv.size()-delim3);
         // cout << "name:" << svName << " year:" << svYear << " contry:" << svCountry << " proj:" << svProject << endl;
         if ( svName.size() > 40 || svCountry.size() > 40 || svCountry.size() < 2) { 
-            cout << "wrong field length:" << svName << " or " << svCountry << endl;
+            // cout << "wrong field length:" << svName << " or " << svCountry << endl;
             continue; 
         }
         if ( ! std::all_of(svName.begin(), svName.end(), [](unsigned char c) { return std::isalpha(c); } ) )  {
-            cout << "wrong name:" << svName << endl;
+            // cout << "wrong name:" << svName << endl;
             continue; 
         }
         int iYearVal = 0;
         std::from_chars_result fcr = std::from_chars(svYear.data(), svYear.data() + svYear.size() , iYearVal );
         if ( iYearVal < 1960 || iYearVal > 2007) {
-            cout << "wrong year:" << iYearVal << endl;
+            // cout << "wrong year:" << iYearVal << endl;
             continue;
         }
 
@@ -46,11 +53,12 @@ std::vector<std::string> generate_report(const std::vector<std::string> & lines)
         }
         // - - - [field ProjectID] - - -
         if ( svProject.size() != 5 || ! std::all_of(svProject.begin(), svProject.begin()+2, [](unsigned char c) { return (c >= 'A' && c <= 'Z');} ) ) {
-            cout << "wrong project format:" << svProject << endl;
+            // cout << "wrong project format:" << svProject << endl;
             continue;
         }    
-        if ( ! std::all_of(svProject.begin()+3, svProject.begin()+5, [](unsigned char c) { return std::isdigit(c); } ) ) {
-            cout << "wrong project format:" << svProject << endl;
+        // cout << "test ProjectID:" << string(svProject.begin()+2, svProject.begin()+5) << endl;
+        if ( ! std::all_of(svProject.begin()+2, svProject.begin()+5, [](unsigned char c) { return (c >= '0' && c<='7'); } ) ) {
+            // cout << "wrong project format:" << svProject << endl;
             continue;
         }   
         // here all ok:
@@ -93,10 +101,9 @@ int main () {
     "Williams1978;none;EF789",
     "Brown;1995;UK;GH0123",
     "Davis;1982;Canada;IJ34"};
-    */
 
     vector<string> vInputData = {
-    "VanLandeghem;1987;SanMarino;MN678",
+    "VanLandeghem;1987;SanMarino;MN678",        // manual input
     "DeConinck;1973;Monaco;OP901",
     "VanDePutte;1990;VaticanCity;QR234",
     "Verstappen;2003;Iceland;ST567",
@@ -108,6 +115,33 @@ int main () {
     "DePaepe;1975;Monaco;EF345",
     "VanHulle;1998;VaticanCity;GH678",
     "Verdonck;2002;Iceland;IJ901"};
+
+*/
+
+    vector<string> vInputData = {
+    "VanLandeghem;1987;SanMarino;MN678",        // copied from HTML page
+    "  DeConinck;1973;Monaco;OP901",
+    "  VanDePutte;1990;VaticanCity;QR234",
+    "  Verstappen;2003;Iceland;ST567 ",
+    "  VanDerAa;1971;Malta;UV890",
+    "  DeBock;1984;Cyprus;WX123",
+    "  VanOvermeire;1996;Andorra;YZ456",
+    "  Verhaeghe;1969;Liechtenstein;AB789",
+    "  VanSteen;1981;SanMarino;CD012",
+    "  DePaepe;1975;Monaco;EF345",
+    "  VanHulle;1998;VaticanCity;GH678",
+    "  Verdonck;2002;Iceland;IJ901"};
+    
+
+/*
+    vector<string> vInputData = {
+        "Smith;1985;USA;AB123",             // copied from HTML page
+        "Johnson;1990;Canada;CD456",
+        "Williams;1978;USA;EF789",
+        "Brown;1995;UK;GH012",
+        "Davis;1982;Canada;IJ345"};
+
+       */ 
 
 
     vector<string> vResult = generate_report(vInputData);
